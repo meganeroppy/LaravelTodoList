76aa6fef8de3e7db057de4ec36c6a7b3135c2c13
meganeroppy <meganeroppy@gmail.com>
2026-03-05 02:03:15 +0900

    Step 5: Localize validation messages to Japanese and add attribute translations

---
+++ lang/ja/validation.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'required' => ':attributeは必ず入力してください。',
+    'max' => [
+        'string' => ':attributeは:max文字以内で入力してください。',
+    ],
+    'attributes' => [
+        'title' => 'タイトル',
+    ],
+];
