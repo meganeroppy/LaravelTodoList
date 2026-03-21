69ab585c39000d81a612f3ba57bd99d9ed4521fc
roppy <meganeroppy@gmail.com>
2026-03-21 14:40:26 +0900

    debug: minimal api for syntax test

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\AuthController;
-use App\Http\Controllers\Api\TodoApiController;
 
-// 公開ルート（ログイン）
-Route::post('/login', [AuthController::class, 'login']);
-
-// 認証済みルート
-Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
-    
-    Route::get('/todos', [TodoApiController.class, 'index']);
-    Route::post('/logout', [AuthController.class, 'logout']);
+Route::get('/ping', function() {
+    return 'pong';
 });
